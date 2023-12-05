a4f97a6bd1d853e74667d9612eb74c7062cedd7f
josep <joselg13067@gmail.com>
2023-12-04 22:19:51 -0500

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,32 +1,47 @@
-let menuVisible = false
-//funcion muestra y oculta el menu
-function iraContacto(){
-    console.log("iraContacto");
+function cambiarColor(numeroColor) {
+  var campo = document.getElementById("idcampo");
+  campo.style.color = "#00ff00";
+  switch (numeroColor) {
+    case 1:
+      console.log("llego el uno");
+      campo.style.color = "#00ff00";
+      break;
+    case 2:
+      console.log("llego el dos");
+      campo.style.color = "#eb9854";
+      break;
+    case 3:
+      console.log("llego el tres");
+      campo.style.color = "#5dddbd";
+      break;
+
+    default:
+      break;
+  }
+  console.log(numeroColor);
 }
 
-function enviarCorreo(){
-    console.log("enviarCorreo");
-}
-function cambiarColor(numeroColor){
-    var campo = document.getElementById('idcampo');
-campo.style.color="#00ff00";
-    switch (numeroColor) {
-		case 1:
-			console.log("llego el uno")
-            campo.style.color="#00ff00";
-			break;
-            case 2:
-			console.log("llego el dos")
-            campo.style.color="#eb9854";
-			break;
-            case 3:
-			console.log("llego el tres")
-            campo.style.color="#5dddbd";
-			break;
+// formulario
+// Intente hacer lo de enviar el mensaje al correo pero no pude
 
-		default:
-			
-			break;	
-	}
-    console.log(numeroColor);
-}
+const btn = document.getElementById("button");
+
+document.getElementById("col").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  btn.value = "Sending...";
+
+  const serviceID = "default_service";
+  const templateID = "template_afs2nmh";
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      alert("Sent!");
+    },
+    (err) => {
+      btn.value = "Send Email";
+      alert(JSON.stringify(err));
+    }
+  );
+});
